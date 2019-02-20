2a06e5a79b047aa7c0337d804112795f28437d9d
Juan Rivera <camilorr_@hotmail.com>
2019-02-20 16:08:09 -0500

    add generic repository

---
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Helpers
+{
+    using System.Threading.Tasks;
+    using Data.Entities;
+    using Microsoft.AspNetCore.Identity;
+
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+    }   
+}
