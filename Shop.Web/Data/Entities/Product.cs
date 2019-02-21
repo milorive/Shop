ff4b4f866a40482260223879e5915ca8020a1113
Juan Rivera <camilorr_@hotmail.com>
2019-02-21 15:55:50 -0500

    add, edit and API images

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -30,5 +30,18 @@
         public double Stock { get; set; }
 
         public User User { get; set; }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://shopmilo.azurewebsites.net{this.ImageUrl.Substring(1)}";
+            }
+        }
     }
 }
